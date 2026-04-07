410555317224ceaae64237904ea66e9849ba99b9
Carlos <prueba@prueba.com>
2026-04-07 11:00:48 +0200

    Creacion de Departamento, EstimacionServices y controller

--- demo/src/main/java/com/example/demo/controller/DetalleEstimacionController.java
+++ demo/src/main/java/com/example/demo/controller/DetalleEstimacionController.java
@@ -1,20 +1,32 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.ApiResponse;
 import com.example.demo.entity.DetalleEstimacion;
 import com.example.demo.repository.DetalleEstimacionRepository;
+import com.example.demo.services.DetalleEstimacionService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.List;
 
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+
 @RestController
-@RequestMapping("/api/detalle_estimacion")
+@RequestMapping("/api/estimaciones")
 @CrossOrigin(origins = "*")
 public class DetalleEstimacionController {
 
     @Autowired
     private DetalleEstimacionRepository detalleEstimacionRepository;
 
+    @Autowired
+    private DetalleEstimacionService detalleEstimacionService;
+    
     @GetMapping("/proyecto/{idProyecto}")
     /* * @PathVariable: Captura el valor que viene en la URL (idProyecto)
      * y lo pasa como argumento al método.
@@ -24,4 +36,22 @@ public class DetalleEstimacionController {
         // Se utiliza el método del repositorio para obtener solo las tareas vinculadas
         return detalleEstimacionRepository.findByIdProyecto(idProyecto);
     }
+
+    @PostMapping("/importar")
+    public ApiResponse importarExcel(@RequestParam("archivo") MultipartFile archivo, @RequestParam("proyectoId") long proyectoId) {
+        
+        if(archivo.isEmpty()) {
+          return  new ApiResponse("El archivo está vacío.", false, null);
+           
+        }
+        try{
+            detalleEstimacionService.procesarExcell(archivo, proyectoId);
+            return new ApiResponse("Archivo procesado exitosamente.", true, null);
+        } catch (Exception e) {
+            return new ApiResponse("Error al procesar el archivo: " + e.getMessage(), false, null);
+        }
+        
+        
+    }
+    
 }
--- demo/src/main/java/com/example/demo/services/DetalleEstimacionService.java
+++ demo/src/main/java/com/example/demo/services/DetalleEstimacionService.java
@@ -1,9 +1,21 @@
 package com.example.demo.services;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.example.demo.entity.DetalleEstimacion;
+import com.example.demo.repository.DepartamentoRepository;
 import com.example.demo.repository.DetalleEstimacionRepository;
 
 @Service
@@ -11,10 +23,64 @@ public class DetalleEstimacionService {
 
     @Autowired
     private DetalleEstimacionRepository detalleEstimacionRepository;
+    @Autowired
+    private DepartamentoRepository departamentoRepository;
 
-    public void procesarExcell(MultipartFile archivo, long proyectoId) {
+    public void procesarExcell(MultipartFile archivo, long proyectoId) throws Exception {
         // Aquí iría la lógica para procesar el archivo Excel y guardar los detalles de estimación
         // en la base de datos utilizando el detalleEstimacionRepository.
+        List<DetalleEstimacion> listaParaGuardar = new ArrayList<>();
+        // Lógica para leer el archivo Excel y convertirlo en objetos DetalleEstimacion
+        try(InputStream flujoDatos = archivo.getInputStream();
+            Workbook miExcel = new XSSFWorkbook(flujoDatos)) {
+         
+                for(int i = 0; i < miExcel.getNumberOfSheets();i++){
+                    Sheet pestanaActual = miExcel.getSheetAt(i);
+                    String nombrePestana = pestanaActual.getSheetName().toUpperCase();
+
+                    int departamentoId = determinarDepartamento(nombrePestana);
+                    if(departamentoId == -1){    
+                        continue; // Si no se reconoce el nombre de la pestaña, se salta a la siguiente
+                    }
+                 for(Row filaActual : pestanaActual){
+                    if(filaActual == null){
+                        continue; // Si la fila es nula, se salta a la siguiente
+                    }
+                    Cell celdaTarea = filaActual.getCell(0); 
+                    Cell celdaMin = filaActual.getCell(3);
+                    Cell celdaMax = filaActual.getCell(4);      
+
+                    if(celdaTarea == null || celdaTarea.getCellType() != CellType.STRING){
+                        continue;
+                    }
+                    if(celdaMin == null || celdaMin.getCellType() != CellType.NUMERIC){
+                        continue;
+                    }
+                    if(celdaMax == null || celdaMax.getCellType() != CellType.NUMERIC){
+                        continue;
+                    }
+
+                    DetalleEstimacion nuevaTarea = new DetalleEstimacion();
+                    nuevaTarea.setIdProyecto(proyectoId);
+                    nuevaTarea.setIdDepartamento(departamentoId);
+                    nuevaTarea.setIdFase(1); // Aquí podrías agregar lógica para determinar la fase si es necesario
+
+                    nuevaTarea.setTarea(celdaTarea.getStringCellValue());
+                    nuevaTarea.setTiempoMin(celdaMin.getNumericCellValue());
+                    nuevaTarea.setTiempoMax(celdaMax.getNumericCellValue());
+
+                    listaParaGuardar.add(nuevaTarea);
+                 }   
+                }
         
     }
+        detalleEstimacionRepository.saveAll(listaParaGuardar);
 }
+
+    private int determinarDepartamento(String nombre){
+
+        return departamentoRepository.findByNombre(nombre).map(departamento -> departamento.getId()).orElse(-1);
+       
+    }
+
+    }
