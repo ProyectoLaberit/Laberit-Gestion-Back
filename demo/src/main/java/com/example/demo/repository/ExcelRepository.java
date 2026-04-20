b2a8845b6226679df45fb0e625788c4a2ac1e5fd
Carlos <prueba@prueba.com>
2026-04-20 11:15:19 +0200

    Cambio ExcelRepositorio

--- demo/src/main/java/com/example/demo/repository/ExcelRepository.java
+++ demo/src/main/java/com/example/demo/repository/ExcelRepository.java
@@ -3,10 +3,19 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Excel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExcelRepository extends JpaRepository<Excel, Integer> {
 
-    Excel findTopByIdProyectoOrderByIdDesc(Long idProyecto);
+   Excel findTopByIdProyectoOrderByIdExcelDesc(Long idProyecto);
+
+    Excel findFirstByIdProyectoAndVigenteTrue(Long idProyecto);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("UPDATE Excel e SET e.vigente = false WHERE e.idProyecto = :idProyecto")
+    void desactivarExcelsAnteriores(@Param("idProyecto") Long idProyecto);
 }
