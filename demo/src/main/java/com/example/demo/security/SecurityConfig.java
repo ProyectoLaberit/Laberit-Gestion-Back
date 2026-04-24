91efab399fe18235f5b3db5118866f80816c8d98
Daniel <danielcc255@gmail.com>
2026-04-24 12:47:35 +0200

    permiso de entrada a todos (momentaneo)

--- demo/src/main/java/com/example/demo/security/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -29,7 +29,8 @@ public class SecurityConfig {
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/usuarios/login").permitAll() // Puerta abierta para el login
-                .anyRequest().authenticated() // CERRADO: Todo lo demás exige estar autenticado
+                .anyRequest().permitAll()
+                //.anyRequest().authenticated() // CERRADO: Todo lo demás exige estar autenticado
             )
             // Añadimos tu filtro antes de la validación por defecto de Spring
             .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
