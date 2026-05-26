a7206df417034259b2c907b815f737a60129e2fa
Nicolás <silviaplestudios@gmail.com>
2026-05-26 13:51:09 +0200

    modificacion obtener errores para excel

--- demo/src/main/java/com/example/demo/services/excel/GeneradorInformeExcelServiceImpl.java
+++ demo/src/main/java/com/example/demo/services/excel/GeneradorInformeExcelServiceImpl.java
@@ -437,7 +437,7 @@ public ByteArrayInputStream generarExcelAnalitico(Long idProyecto, Integer idExc
 
     public ProblemasDetectadosDTO obtenerProblemasDetectados(Long idProyecto, int idExcel){
 
-        List<GitLabTarea> tareasBDINvalidas = gitLabTareaRepository.findByValidaAndTareaProyecto_IdProyecto(false, idProyecto);
+        List<GitLabTarea> tareasBDINvalidas = gitLabTareaRepository.findByValidaAndProyectoId_Id(false, idProyecto);
         int numeroInvalidasGit = tareasBDINvalidas.size();
 
         List<ImputacionClockify> imputacionesBD = imputacionClockifyRepository.findByIdProyectoAndValida(idProyecto, false);
