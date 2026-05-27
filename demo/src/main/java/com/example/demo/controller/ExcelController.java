a6820069a615f559a08791257ccb077b87aac24c
Carlos <prueba@prueba.com>
2026-05-27 11:40:24 +0200

    añadir campo idExcel

--- demo/src/main/java/com/example/demo/controller/ExcelController.java
+++ demo/src/main/java/com/example/demo/controller/ExcelController.java
@@ -68,8 +68,8 @@ public class ExcelController {
         entidad = Excel.class, // Ajusta si la entidad auditada principal debe ser Proyecto.class
         descripcion = "Se descargó el Reporte Analítico del Proyecto ID: #idProyecto"
     )
-    @GetMapping("/exportar-analitico/{idProyecto}")
-    public ResponseEntity<byte[]> exportarExcelAnalitico(@PathVariable Long idProyecto, @RequestParam(required = false) Integer idExcel) {
+    @GetMapping("/exportar-analitico/{idProyecto}/{idExcel}")
+    public ResponseEntity<byte[]> exportarExcelAnalitico(@PathVariable Long idProyecto, @PathVariable Integer idExcel) {
         try {
             // Llamamos a nuestro nuevo servicio
             ByteArrayInputStream bis = generadorInformeExcelService.generarExcelAnalitico(idProyecto, idExcel);
